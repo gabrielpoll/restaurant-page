54d6ee5cb2e1947cb6b94744574160f7714004c2
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-02-07 15:50:43 -0300

    configure webpack asset and plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { type } from "node:os";
 
 export default {
     mode: "development",
@@ -8,4 +10,27 @@ export default {
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: "./src/template.html",
+            }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+
+    }
 };
