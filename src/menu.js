d223f96d66a5d34fbf74f21790e3d952c9e2d1e6
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-02-07 17:18:49 -0300

    create first plate in the manu.js

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,27 @@
+import pastaAlVesuvioImage from "./pasta-el-vesuvio.png";
+
 const divContent = document.getElementById("content");
 
 const loadMenu = () => {
-    divContent.textContent = "menu";
 
+    divContent.textContent = "";
 
+    const menuTitle = document.createElement("div");
+    const textTitle = document.createElement("h1");
+    textTitle.textContent = "Menu";
+    menuTitle.appendChild(textTitle);
+
+    const firstPlate = document.createElement("div");
+    const pastaAlVesuvioTitle = document.createElement("p");
+    const pastaPrice = document.createElement("p");
+    const pastaImage = document.createElement("img");
+    pastaAlVesuvioTitle.textContent = "Pasta El Vesuvio";
+    pastaPrice.textContent = "$4.99"
+    pastaImage.src = pastaAlVesuvioImage;
+    firstPlate.append(pastaAlVesuvioTitle, pastaPrice, pastaImage);
+    
+
+    divContent.append(menuTitle, firstPlate);
 };
 
 export default loadMenu;
